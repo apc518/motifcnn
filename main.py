0f01c4e78cdad0e5aaa8e1cebc960ff161928545
apc518 <andychamberlainmusic@gmail.com>
2022-02-09 03:45:02 -0800

    main.py updates - uses the new version of convert_audio_to_spectrogram - normalizes input audio - takes in an optional second command line argument for the model path (makes comparing different models easier) - added some more things to the gitignore

--- main.py
+++ main.py
@@ -1,34 +1,52 @@
+"""
+Usage: python main.py <pathtoaudio> [<pathtomodel>]
+"""
+
+
 import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+import random
+import sys
+import io
+
 import tensorflow as tf
 from tensorflow import keras
 import librosa
-from PIL import Image
 import librosa.display
 import matplotlib.pyplot as plt
-import sys
 import numpy as np
+from PIL import Image
+from pydub import AudioSegment, effects
 
+from augment import audiosegment_to_numpy_array
 
-def convert_audio_to_spectogram(filename, outfilename=None, overwrite=False):
+def convert_audio_to_spectogram(filepath):
     """
-    convert_audio_to_spectogram -- using librosa to plot and save a spectrogram
-
-    Arguments:
-    filename -- filepath to the file that you want to see the waveplot for
-    outfilename -- filepath to the output spectrogram (must be something matplotlib.pyplot.savefig() can handle)
-    overwrite -- whether to overwrite if a file already exists with the given outfilename
-
-    Returns -- None
+    ## create a spectrogram from an audio file
+    
+    #### Arguments:
+    - filepath -- path to the audio file of which a spectrogram will be made
+    
+    #### Returns
+    - A PIL.Image object of the spectrogram
     """
 
-    # sr == sampling rate
-    audio_data, sr = librosa.load(filename, sr=44100)
+    # load sound with pydub
+    snd = AudioSegment.from_file(filepath)
+    snd = effects.normalize(snd)
+
+    # sr == sample rate 
+    sr = snd.frame_rate
+    audio_data = audiosegment_to_numpy_array(snd)
+
+    # pitch shift by random amount for additional augmentation
+    audio_data = librosa.effects.pitch_shift(audio_data, sr, random.randint(-6, 12))
 
     vertical_res = 4096
-
+    
     # stft is short time fourier transform
     sgram = librosa.stft(audio_data, center=False, n_fft=vertical_res, win_length=vertical_res)
-
+    
     # convert the slices to amplitude
     sgram_db = librosa.amplitude_to_db(abs(sgram))
 
@@ -37,22 +55,19 @@ def convert_audio_to_spectogram(filename, outfilename=None, overwrite=False):
     librosa.display.specshow(sgram_db, sr=sr, x_axis='time', y_axis='log', ax=ax, cmap='gray')
 
     # plt.gca().set_axis_off()
-    plt.subplots_adjust(top=1, bottom=0, right=1, left=0,
-                        hspace=0, wspace=0)
-    plt.margins(0, 0)
+    plt.subplots_adjust(top = 1, bottom = 0, right = 1, left = 0,
+                hspace = 0, wspace = 0)
+    plt.margins(0,0)
 
-    if outfilename in os.listdir() and not overwrite:
-        print("Given filename already exists, to overwrite pass in argument `overwrite=True`")
-        return
-    if outfilename is not None:
-        plt.savefig(outfilename)
+    img_buf = io.BytesIO()
+    plt.savefig(img_buf, format="png")
+    plt.close() # dont display the spectrogram on screen, and dont leak memory
 
-        # crop the image to get rid of useless high and low frequencies
-        im = Image.open(outfilename)
-        im_cropped = im.crop((0, 90, 360, 315))
-        im_cropped.save(outfilename)
-
-    plt.close()  # dont display the spectrogram on screen
+    # crop the image to get rid of useless high and low frequencies
+    im = Image.open(img_buf)
+    im_cropped = im.crop((0, 150, 500, 450))
+    im_resized = im_cropped.resize((100, 300))
+    return im_resized
 
 
 # Press the green button in the gutter to run the script.
@@ -61,31 +76,35 @@ if __name__ == '__main__':
         Assume there is a model in the directory named model.tf
     """
 
+    # import warnings
+    # warnings.simplefilter("ignore")
+
+
     # load model
     model_path = "./model.tf"
+    if len(sys.argv) > 2:
+        model_path = sys.argv[2]
+
     if os.path.exists(model_path):
         model = keras.models.load_model(model_path)
     else:
-        print("Model not found in directory\n")
-        model_abs_path = input("Please input an absolute path")
-        while os.path.exists(model_abs_path):
-            print("Model not found in directory\n")
-            model_abs_path = input("Please input an absolute path")
+        print(f"Model directory \"{model_path}\" not found.")
+        exit(1)
+        
     # Verify audio
     audio_path = ""
 
-    if len(sys.argv) == 2 and os.path.exists(sys.argv[1]):
+    if os.path.exists(sys.argv[1]):
         audio_path = sys.argv[1]
     else:
-        while os.path.exists(audio_path):
-            audio_path = input("[Usage] filepath: ")
+        print("Audio file not found.")
+        exit(1)
 
-    print("Have the audio loaded")
     # create spectrogram
-    convert_audio_to_spectogram("./" + audio_path, "./spec.png")
+    convert_audio_to_spectogram(audio_path).save("./spec.png")
 
     # preprocess the spectrogram
-    image = tf.keras.preprocessing.image.load_img("./spec.png", target_size=(360, 225))
+    image = tf.keras.preprocessing.image.load_img("./spec.png", target_size=(100, 300))
     input_arr = tf.keras.preprocessing.image.img_to_array(image)
     input_arr = np.array([input_arr])
 
@@ -93,17 +112,15 @@ if __name__ == '__main__':
     # TODO: add prediction confidence
     # TODO: verify that yes is 1 and no is 0
     predictions = model.predict(input_arr)
-    print(f'Prediction returns: {predictions}')
+    print(f'{predictions=}')
+    # print(f'{model.classnames}')
+
     if np.argmax(predictions[0]) == 1:
         print(f'Yes with confidence {predictions[0][1]}')
     elif np.argmax(predictions) == 0:
         print(f'No with confidence {predictions[0][0]}')
     else:
-        print("an Error has occurred")
+        print("an error has occurred")
 
-
-    # clean up and exit
+    # clean up
     os.remove("./spec.png")
-    exit(0)
-
-
